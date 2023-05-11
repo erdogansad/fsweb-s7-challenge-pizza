f424b9bda172ba1bd57a06098deef4e5b38888ce
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-05-11 06:12:57 +0300

    optimization update

---
+++ src/utils/Images.js
@@ -0,0 +1,31 @@
+import homeSectionBackground from "../assets/img/mvp-banner.png";
+import ramenImg from "../assets/img/adv-aseets/icons/1.svg";
+import pizzaImg from "../assets/img/adv-aseets/icons/2.svg";
+import burgerImg from "../assets/img/adv-aseets/icons/3.svg";
+import friesImg from "../assets/img/adv-aseets/icons/4.svg";
+import fastFoodImg from "../assets/img/adv-aseets/icons/5.svg";
+import softDrinksImg from "../assets/img/adv-aseets/icons/6.svg";
+import cardOneImg from "../assets/img/adv-aseets/kart-1.png";
+import cardTwoImg from "../assets/img/adv-aseets/kart-2.png";
+import cardThreeImg from "../assets/img/adv-aseets/kart-3.png";
+import foodOne from "../assets/img/adv-aseets/food-1.png";
+import foodTwo from "../assets/img/adv-aseets/food-2.png";
+import foodThree from "../assets/img/adv-aseets/food-3.png";
+import headerImg from "../assets/img/adv-aseets/adv-form-banner.png"
+
+export { 
+    homeSectionBackground,
+    ramenImg,
+    pizzaImg,
+    burgerImg,
+    friesImg,
+    fastFoodImg,
+    softDrinksImg,
+    cardOneImg,
+    cardTwoImg,
+    cardThreeImg,
+    foodOne,
+    foodTwo,
+    foodThree,
+    headerImg
+ }
